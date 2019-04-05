ba22a1b535d1de0109459a5b1f08f32a8297aba9
Andreas Müller <andreas_mueller@live.com>
2019-04-05 18:25:31 +0200

    Use mockingoose to mock mongoose and adapt test

--- server/resolvers/index.spec.ts
+++ server/resolvers/index.spec.ts
@@ -1,6 +1,15 @@
+import mockingoose from 'mockingoose';
 import resolvers from './index';
+import Meta from "../persistance/Meta";
 
+test('basic', async () => {
+    const _doc = {
+        name: 'QuizDB',
+        version: '1.0',
+    };
+    mockingoose(Meta).toReturn(_doc, 'findOne');
 
-test('basic', () => {
-    expect(resolvers.Query.info()).toBe('Hello from GraphQL');
+    const result = await resolvers.Query.info();
+
+    expect(result).toBe('Hello from GraphQL and QuizDB v1.0');
 });
