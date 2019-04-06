c82bd8bc474cd3f9e73b9aef2cecf9a2d9cf245f
danielsuter <suter_daniel@hotmail.com>
2019-04-06 16:09:13 +0200

    Correct schema. Do not start mongo db if no uri is defined (local development).

--- server/app.ts
+++ server/app.ts
@@ -6,9 +6,13 @@ import MongoClient from "./persistance/MongoClient";
 
 // Connect to MongoDB
 const mongoUri = <string>process.env.MONGODB_URI;
-const mongoClient = new MongoClient();
-mongoClient.connectDb(mongoUri)
-   .then(() => mongoClient.initDummyData());
+if(mongoUri) {
+    const mongoClient = new MongoClient();
+    mongoClient.connectDb(mongoUri)
+        .then(() => mongoClient.initDummyData());
+} else {
+    console.log(`Environment variable MONGO_URI not defined. Not connecting to MongoDB`);
+}
 
 const server = new GraphQLServer({
     typeDefs: './schema.graphql',
@@ -16,7 +20,7 @@ const server = new GraphQLServer({
 });
 
 // Use underlying express server
-server.express.use(express.static(path.join(__dirname, '../frontend/build')));
+server.express.use(express.static(path.join(__dirname, '../../frontend/build')));
 
 const options = {
     port: process.env.PORT || 4000,
@@ -24,4 +28,5 @@ const options = {
     subscriptions: '/subscriptions',
     playground: '/playground'
 };
-server.start(options, ({port}) => console.log(`Server is running on http://localhost:${port}`));
+server.start(options, ({port}) => console.log(`Server is running on http://localhost:${port}
+Playground running at http://localhost:${port}${options.playground}`));
--- server/resolvers/index.ts
+++ server/resolvers/index.ts
@@ -1,11 +1,17 @@
-import {Context} from "graphql-yoga/dist/types";
-import Meta, {IMeta} from "../persistance/Meta";
+import {Context} from 'graphql-yoga/dist/types';
+import Meta, {IMeta} from '../persistance/Meta';
 
 export default {
     Query: {
         info: async (parent?: any, args?: any, context?: Context) => {
             const meta = <IMeta>await Meta.findOne();
             return `Hello from GraphQL and ${meta.name} v${meta.version}`;
-        },
+        }
+    },
+    Mutation: {
+        addQuiz: async (parent: any, args: any) => {
+            console.log(JSON.stringify(args));
+            return undefined;
+        }
     }
 }
