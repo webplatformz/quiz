5ccd54013b8e189a5b4bd23fa52f55dad56ffa1c
danielsuter <suter_daniel@hotmail.com>
2019-04-08 13:49:50 +0300

    Switch from graphql yoga to apollo server using express as middle ware.

--- server/resolvers/index.ts
+++ server/resolvers/index.ts
@@ -1,21 +1,20 @@
-import {Context} from 'graphql-yoga/dist/types';
 import {isMongoDbEnabled} from '../persistence';
 import MongoDbRepository from '../repositories/mongdb-repository';
 import {Quiz} from '../domain/quiz';
 
 export default {
     Query: {
-        info: async (parent?: any, args?: any, context?: Context) => {
+        info: async (parent?: any, args?: any) => {
             if (isMongoDbEnabled()) {
                 const meta = await MongoDbRepository.getMeta();
                 return `Hello from GraphQL and ${meta.name} v${meta.version}`;
             } else {
-                return 'Hello from GraphQL'
+                return 'Hello from GraphQL 2'
             }
         }
     },
     Mutation: {
-        addQuiz: async (parent: any, {input}: {input: Quiz}) => {
+        createQuiz: async (parent: any, {input}: {input: Quiz}) => {
             console.log(JSON.stringify(input));
             input.id = 1;
             return input;
