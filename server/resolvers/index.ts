45af1fc0121772f8bcaff626ff4042f0f04608e0
danielsuter <suter_daniel@hotmail.com>
2019-04-08 13:51:48 +0300

    Remove mongodb from info to speed up development.

--- server/resolvers/index.ts
+++ server/resolvers/index.ts
@@ -5,16 +5,11 @@ import {Quiz} from '../domain/quiz';
 export default {
     Query: {
         info: async (parent?: any, args?: any) => {
-            if (isMongoDbEnabled()) {
-                const meta = await MongoDbRepository.getMeta();
-                return `Hello from GraphQL and ${meta.name} v${meta.version}`;
-            } else {
-                return 'Hello from GraphQL 2'
-            }
+            return 'Hello from GraphQL 2'
         }
     },
     Mutation: {
-        createQuiz: async (parent: any, {input}: {input: Quiz}) => {
+        createQuiz: async (parent: any, {input}: { input: Quiz }) => {
             console.log(JSON.stringify(input));
             input.id = 1;
             return input;
