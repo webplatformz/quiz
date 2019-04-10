375b67d1c9540ec87bed903bfd5331e27490c2a5
Martin Leimer <martin.leimer@zuehlke.com>
2019-04-10 08:41:58 +0200

    added remaining subscriptions and adjusted onQuestionTimeout parameter from operatorId to joinId

--- frontend/src/components/pages/quiz/QuizContainer.tsx
+++ frontend/src/components/pages/quiz/QuizContainer.tsx
@@ -6,19 +6,49 @@ import {withApollo, WithApolloClient} from "react-apollo";
 import {gql} from "apollo-boost";
 import PlayerList from "./PlayerList";
 import {Grid, Cell} from 'react-mdl';
+import {Question} from "../../../../../server/domain/question";
+import {Answer} from "../../../../../server/domain/answer";
+import {Ranking} from "../../../../../server/domain/ranking";
 
 interface QuizContainerState {
     joinId: string,
-    players: Player[]
+    players: Player[],
+    question?: Question,
+    answer?: Answer,
+    ranking?: Ranking
 }
 
 const PLAYER_JOIN_SUBSCRIPTION = gql`
     subscription onPlayerJoined($joinId: String!){
         onPlayerJoined(joinId: $joinId) {
-          players {
-            id
-            name
-          }
+            players {
+                id
+                name
+            }
+        }
+    }
+`;
+
+const NEXT_QUESTION_SUBSCRIPTION = gql`
+    subscription onNextQuestion($joinId: String!){
+        onNextQuestion(joinId: $joinId) {
+            question
+        }
+    }
+`;
+
+const QUESTION_TIMEOUT_SUBSCRIPTION = gql`
+    subscription onQuestionTimeout($joinId: String!){
+        onQuestionTimeout(joinId: $joinId) {
+            answer
+        }
+    }
+`;
+
+const RANKING_CHANGED_SUBSCRIPTION = gql`
+    subscription onRankingChanged($joinId: String!){
+        onRankingChanged(joinId: $joinId) {
+            ranking
         }
     }
 `;
@@ -26,7 +56,9 @@ const PLAYER_JOIN_SUBSCRIPTION = gql`
 class QuizContainer extends Component<WithApolloClient<any>, QuizContainerState> {
     state: QuizContainerState = {
         joinId: "",
-        players: []
+        players: [],
+        question: undefined,
+        ranking: undefined
     };
 
     constructor(props: any) {
@@ -47,6 +79,42 @@ class QuizContainer extends Component<WithApolloClient<any>, QuizContainerState>
             }
         }).subscribe((response: any) => {
             this.setState({...this.state, players: response.data.onPlayerJoined.players});
+        })
+    }
+
+    // TODO: subscribe to me
+    subscribeToNextQuestion(): void {
+        this.props.client.subscribe({
+            query: NEXT_QUESTION_SUBSCRIPTION,
+            variables: {
+                joinId: this.state.joinId
+            }
+        }).subscribe((response: any) => {
+            this.setState({...this.state, question: response.data.onNextQuestion.question});
+        })
+    }
+
+    // TODO: subscribe to me
+    subscribeToQuestionTimeout(): void {
+        this.props.client.subscribe({
+            query: QUESTION_TIMEOUT_SUBSCRIPTION,
+            variables: {
+                joinId: this.state.joinId
+            }
+        }).subscribe((response: any) => {
+            this.setState({...this.state, answer: response.data.onQuestionTimeout.answer});
+        })
+    }
+
+    // TODO: subscribe to me
+    subscribeToRankingChanged(): void {
+        this.props.client.subscribe({
+            query: RANKING_CHANGED_SUBSCRIPTION,
+            variables: {
+                joinId: this.state.joinId
+            }
+        }).subscribe((response: any) => {
+            this.setState({...this.state, ranking: response.data.onRankingChanged.ranking});
         })
     }
 
