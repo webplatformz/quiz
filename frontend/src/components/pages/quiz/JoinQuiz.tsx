9cd01f81b3da34b1df9928e2b19c8a43e1154cc7
Andreas Müller <andreas_mueller@live.com>
2019-04-12 10:53:13 +0300

    Add error toast message

--- frontend/src/components/pages/quiz/JoinQuiz.tsx
+++ frontend/src/components/pages/quiz/JoinQuiz.tsx
@@ -1,8 +1,9 @@
 import React, {Component} from 'react'
 import {gql} from 'apollo-boost';
 import {withApollo, WithApolloClient} from 'react-apollo';
-import {Button, Textfield, Card, CardTitle, CardText} from 'react-mdl';
+import {Button, Textfield} from 'react-mdl';
 import {Player} from '../../../../../server/domain/player';
+import {withToastManager} from 'react-toast-notifications';
 
 interface JoinQuizState {
     joinId: string,
@@ -11,6 +12,7 @@ interface JoinQuizState {
 
 interface JoinQuizProps {
     joinQuiz: (joinId: string, playerId: string, players: Player[]) => void
+    toastManager: any
 }
 
 const JOIN_MUTATION = gql`
@@ -39,17 +41,27 @@ class JoinQuiz extends Component<WithApolloClient<JoinQuizProps>, JoinQuizState>
 
     joinQuiz(): void {
         this.props.client.mutate({
-                mutation: JOIN_MUTATION,
-                variables: {
-                    joinId: this.state.joinId,
-                    nickname: this.state.nickname
-                }
-            })
+            mutation: JOIN_MUTATION,
+            variables: {
+                joinId: this.state.joinId,
+                nickname: this.state.nickname
+            }
+        })
             .then((response: any) => this.props.joinQuiz(
                 response.data.join.joinId,
                 response.data.join.playerId,
                 response.data.join.players)
-            );
+            )
+            .catch(() => {
+                this.showErrorToast("No quiz with this ID found. Please check if your ID is correct.")
+            });
+    }
+
+    showErrorToast(message: string) {
+        this.props.toastManager.add(message, {
+            appearance: 'error',
+            autoDismiss: true,
+        })
     }
 
     render() {
@@ -57,13 +69,13 @@ class JoinQuiz extends Component<WithApolloClient<JoinQuizProps>, JoinQuizState>
             <div>
                 <h4 style={{marginTop: 0}}>Join Quiz</h4>
                 <Textfield onChange={(event: React.FormEvent<HTMLInputElement>) => {
-                                this.setState({...this.state, joinId: event.currentTarget.value})
-                            }}
+                    this.setState({...this.state, joinId: event.currentTarget.value})
+                }}
                            label="Quiz ID"
                            pattern="^[A-Za-z1-9]{0,6}$"/>
                 <Textfield onChange={(event: React.FormEvent<HTMLInputElement>) => {
-                                this.setState({...this.state, nickname: event.currentTarget.value})
-                            }}
+                    this.setState({...this.state, nickname: event.currentTarget.value})
+                }}
                            label="Nickname"/>
                 <br/>
                 <Button raised ripple colored style={{margin: '10px'}} onClick={this.joinQuiz}>Join</Button>
@@ -72,4 +84,4 @@ class JoinQuiz extends Component<WithApolloClient<JoinQuizProps>, JoinQuizState>
     }
 }
 
-export default withApollo(JoinQuiz);
+export default withToastManager(withApollo(JoinQuiz));
